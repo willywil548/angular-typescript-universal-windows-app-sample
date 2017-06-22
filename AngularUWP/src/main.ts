5507c73d3455ac1f2e5b4711efe56caba39d8dc2
Manikanta Reddy D <manikanta.reddy.d@outlook.com>
2017-06-22 09:52:38 +0530

    fixing ng build

--- AngularUWP/src/main.ts
+++ AngularUWP/src/main.ts
@@ -1,5 +1,4 @@
 // main entry point
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-
 platformBrowserDynamic().bootstrapModule(AppModule);
